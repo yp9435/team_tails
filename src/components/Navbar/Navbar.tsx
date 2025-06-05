70d4ea6e4b47c64f762e78c65de8f5c097620a1c
yp9435 <147942578+yp9435@users.noreply.github.com>
2025-06-06 02:07:10 +0530

    update

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Navbar: React.FC = () => (
-  <nav className="w-full flex items-center justify-between px-8 py-4 bg-white shadow">
-    <span className="text-2xl font-bold text-orange-500">TeamTails</span>
-    <span className="text-gray-400 text-base font-light">HappyFox Frontend assignment</span>
+  <nav className="w-full flex items-center justify-between px-8 py-4 bg-orange-500 shadow">
+    <span className="text-2xl font-bold text-white">TeamTails</span>
+    <span className="text-gray-200 text-base font-light">HappyFox Frontend assignment</span>
   </nav>
 );
 
