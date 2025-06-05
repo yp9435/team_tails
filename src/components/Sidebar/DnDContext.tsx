05312730467d7ee0176c0294d50682b51de80dc7
yp9435 <147942578+yp9435@users.noreply.github.com>
2025-06-06 02:48:06 +0530

    Update DnDContext.tsx

--- src/components/Sidebar/DnDContext.tsx
+++ src/components/Sidebar/DnDContext.tsx
@@ -1,24 +1,21 @@
 import { createContext, useContext, useState } from 'react';
- 
-
-type DnDContextValue = [string | null, (type: string | null) => void];
-
-const DnDContext = createContext<DnDContextValue>([null, () => {}]);
- 
+import type { Employee } from '../../types/employee';
 import type { ReactNode } from 'react';
 
+const DnDContext = createContext<[Employee | null, (e: Employee | null) => void]>([null, () => {}]);
+
 export const DnDProvider = ({ children }: { children: ReactNode }) => {
-  const [type, setType] = useState<string | null>(null);
- 
+  const [type, setType] = useState<Employee | null>(null);
+
   return (
     <DnDContext.Provider value={[type, setType]}>
       {children}
     </DnDContext.Provider>
   );
-}
- 
+};
+
 export default DnDContext;
- 
+
 export const useDnD = () => {
   return useContext(DnDContext);
-}
+};
